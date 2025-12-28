eb8e4f94050e15ffb7766036a8f44f2e01734316
davidcastilloalvarado <dcastilloa@uni.pe>
2025-12-28 18:20:58 -0500

    fix: mock url bypass

--- etls/sinadef-analysis/src/pipeline.py
+++ etls/sinadef-analysis/src/pipeline.py
@@ -41,9 +41,32 @@ def extract_data(input_path: str) -> pd.DataFrame:
     # Create data directory if it doesn't exist
     os.makedirs("./data", exist_ok=True)
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'en-US,en;q=0.9',
+        'priority': 'u=0, i',
+        'sec-ch-ua': '"Google Chrome";v="143", "Chromium";v="143", "Not A(Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Linux"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36',
     }
-    response = requests.get(url, headers=headers)
+
+    cookies = {
+        '_ga_2VDX9N2E9M': 'GS2.1.s1766243911$o1$g0$t1766243913$j58$l0$h0',
+        '_ga': 'GA1.1.522129251.1766241622',
+        '__Host-nc_sameSiteCookielax': 'true',
+        '__Host-nc_sameSiteCookiestrict': 'true',
+        '_ga_F3MSER51W2': 'GS2.1.s1766280361$o3$g0$t1766280366$j55$l0$h0',
+        '_ga_H4TV4M96SY': 'GS2.1.s1766294636$o4$g0$t1766294636$j60$l0$h0',
+        'ocys5xdczo5m': '45fec6c6e200c7b283a4cfa233e1cc54',
+        'oc_sessionPassphrase': '2yPaYiOujtOAtS1qb1SOPbhmit0Caud9wcYPvd4A7PTX4wlhtUpWt2%2BLYCQ03kd43mWYAGyks%2BRziqbhiu3ny%2FuO6a6EmpDABhDclLiF7LxpgM7xiCsiRsq24Tga6%2Fuv',
+        '__cf_bm': 'HaVDlhD6lwmjC1W.yb99MNkU1Fp4dIIcywFv2hheBM4-1766963090-1.0.1.1-Ay6uRG5H4TBZcxAykROodEois6pF9GYte_usP9NkpsBfylZ7a872vISUmVxsaDF3DqSoEnuzSC9SL7OIvDg9qIS4fawOf.2z0NvxdcBIBVg',
+    }
+    response = requests.get(url, headers=headers, cookies=cookies)
 
     # Check response status
     if response.status_code != 200:
